6fbcdcb0c254c2cf0356e85f23cca452c58fadbe
Ricardo Neves - Contractor <RNeves@euronext.com>
2026-05-21 17:47:59 +0100

    create VehiclesController

---
+++ VehicleApi/Controllers/VehiclesController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VehicleApi.Interfaces;
+using VehicleApi.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace VehicleApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VehiclesController : ControllerBase
+    {
+        private readonly IVehicle _vehicleService;
+
+        public VehiclesController(IVehicle vehicleService)
+        {
+            _vehicleService = vehicleService;
+        }
+
+        // GET: api/<VehiclesController>/5
+        [HttpGet]
+        public async Task<IEnumerable<Vehicle>> Get()
+        {
+            return await _vehicleService.GetAllVehicles();
+        }
+
+        // GET api/<VehiclesController>/5
+        [HttpGet("{id}")]
+        public async Task<Vehicle> Get(int id)
+        {
+            return await _vehicleService.GetVehicleById(id);
+        }
+
+        // POST api/<VehiclesController>
+        [HttpPost]
+        public async Task Post([FromBody] Vehicle vehicle)
+        {
+            await _vehicleService.AddVehicle(vehicle);
+        }
+
+        // PUT api/<VehiclesController>/5
+        [HttpPut("{id}")]
+        public async Task Put(int id, [FromBody] Vehicle vehicle)
+        {
+            await _vehicleService.UpdateVehicle(id, vehicle);
+        }
+
+        // DELETE api/<VehiclesController>/5
+        [HttpDelete("{id}")]
+        public async Task Delete(int id)
+        {
+            await _vehicleService.DeleteVehicle(id);
+        }
+    }
+}
