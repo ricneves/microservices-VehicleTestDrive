4faffe2fa3c207eacf0423715708aeeac865dc44
Ricardo Neves - Contractor <RNeves@euronext.com>
2026-05-25 17:53:48 +0100

    customerservice

--- CustomersApi/Program.cs
+++ CustomersApi/Program.cs
@@ -1,3 +1,7 @@
+using CustomersApi.Interfaces;
+using CustomersApi.Models;
+using CustomersApi.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<ICustomer, CustomerService>();
 
 var app = builder.Build();
 
---
+++ CustomersApi/Services/CustomerService.cs
@@ -0,0 +1,29 @@
+﻿using CustomersApi.Data;
+using CustomersApi.Interfaces;
+using CustomersApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CustomersApi.Services;
+
+public class CustomerService : ICustomer
+{
+    private ApiDbContext dbContext;
+
+    public CustomerService()
+    {
+        dbContext = new ApiDbContext();
+    }
+
+    public async Task AddCustomer(Customer customer)
+    {
+        var vehicleInDb = await dbContext.Vehicles.FirstOrDefaultAsync(v => v.Id == customer.VehicleId);
+        if (vehicleInDb == null) 
+        {
+            await dbContext.Vehicles.AddAsync(customer.Vehicle);
+            await dbContext.SaveChangesAsync();
+        }
+        customer.Vehicle = null;
+        await dbContext.Customers.AddAsync(customer);
+        await dbContext.SaveChangesAsync();
+    }
+}
