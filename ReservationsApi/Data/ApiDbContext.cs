51c79474845d101487c709a5c8b61c87d92ad1b9
ricneves <ricardoaneves@gmail.com>
2026-05-25 23:50:18 +0100

    reservations

---
+++ ReservationsApi/Data/ApiDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReservationsApi.Models;
+
+namespace ReservationsApi.Data;
+
+public class ApiDbContext : DbContext
+{
+    public DbSet<Vehicle> Vehicles { get; set; }
+    public DbSet<Reservation> Reservations { get; set; }
+
+    override protected void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=ReservationApiDb;");
+    }
+}
