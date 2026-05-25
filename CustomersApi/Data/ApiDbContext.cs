bb13f39f306b7b13c4c4160d594c0369c6171724
ricneves <ricardoaneves@gmail.com>
2026-05-25 21:58:28 +0100

    Develop (#1)
    * create projects api
    
    * initial database
    
    * create VehicleService
    
    * create VehiclesController
    
    * Delete CustomersApi/CustomersApi.csproj.user
    
    * Delete ReservationsApi/ReservationsApi.csproj.user
    
    * create CustomerApiDb
    
    * create ICustomer
    
    * customerservice
    
    ---------
    
    Co-authored-by: Ricardo Neves - Contractor <RNeves@euronext.com>

---
+++ CustomersApi/Data/ApiDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CustomersApi.Models;
+
+namespace CustomersApi.Data;
+
+public class ApiDbContext : DbContext
+{
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Vehicle> Vehicles { get; set; }
+
+    override protected void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=CustomerApiDb;");
+    }
+}
---
+++ VehicleApi/Data/ApiDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VehicleApi.Models;
+
+namespace VehicleApi.Data;
+
+public class ApiDbContext : DbContext
+{
+    public DbSet<Vehicle> Vehicles { get; set; }
+
+    override protected void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=VehicleApiDb;");
+    }
+}
