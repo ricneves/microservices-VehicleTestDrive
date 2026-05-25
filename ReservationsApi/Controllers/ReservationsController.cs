d63304e5649d4ec516dfe1d7d4802f886d8a9134
ricneves <ricardoaneves@gmail.com>
2026-05-26 00:23:18 +0100

    ReservationsController

---
+++ ReservationsApi/Controllers/ReservationsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ReservationsApi.Interfaces;
+using ReservationsApi.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ReservationsApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReservationsController : ControllerBase
+    {
+        private readonly IReservation _reservationService;
+
+        public ReservationsController(IReservation reservationService)
+        {
+            _reservationService = reservationService;
+        }
+        // GET: api/<ReservationsController>
+        [HttpGet]
+        public async Task<IEnumerable<Reservation>> Get()
+        {
+            return await _reservationService.GetReservations();
+        }
+
+        // PUT api/<ReservationsController>/5
+        [HttpPut("{id}")]
+        public async Task Put(int id)
+        {
+            await _reservationService.UpdateMailStatus(id);
+        }
+    }
+}
